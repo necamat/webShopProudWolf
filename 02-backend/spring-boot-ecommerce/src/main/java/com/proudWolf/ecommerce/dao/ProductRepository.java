d40a6917e618bb0480066538e02ff1046fa244ee
necamat <matovicnemanja85@gmail.com>
2021-08-24 00:49:15 +0200

    Front End. Created a basic Angular app. And solved CORS policy

--- 02-backend/spring-boot-ecommerce/src/main/java/com/proudWolf/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/proudWolf/ecommerce/dao/ProductRepository.java
@@ -3,8 +3,12 @@ package com.proudWolf.ecommerce.dao;
 
 import com.proudWolf.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+/*
+** http://localhost:4200 is actual origin that our Angular application is running on. Currently!!! (not in production)
+*/
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
     
 }
